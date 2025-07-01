759573bd5010666b1c38e70146c9e3229b366ec5
almaz990 <almazz2f@gmail.com>
2025-07-01 19:48:57 +0300

    Add files via upload

---
+++ lab01/1_1/Mukhametshin_1_1.py
@@ -0,0 +1,6 @@
+var1 = None
+var2 = True
+var3 = 1
+var4 = 5.7
+var5 = 5+4j
+print(type(var1), type(var2), type(var3), type(var4), type(var5))
---
+++ lab01/1_1/Mukhametshin_1_3.py
@@ -0,0 +1,3 @@
+cost_per_item = 230.
+number = 50
+print("The total cost of " + str(number) + " item is " + str(cost_per_item * number))
